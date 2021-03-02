74e0af9415f0aee49c700abb88693654523e366f
ariefdfaltah <arief.dfaltah@gmail.com>
2021-03-02 15:29:01 +0700

    add createSignature and validate

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-module.exports = function() {
-    console.log("you're wonderful!");
-    return;
-};
+const MD5 = require('crypto-js/md5')
+const crypto = require('crypto')
+
+class HMACSignature {
+    _createHash(request, APIContext, timestamp) {
+        const { body, method } = request
+        const js = JSON.stringify(body)
+        const jsc =  js.replace(/\s+/g, '')
+        const MD5jsc = MD5(jsc).toString()
+        const rw = `${timestamp}:${APIContext.key}:${method}:${MD5jsc}`
+        return crypto.createHmac('sha256', APIContext.secret).update(rw).digest('base64')
+    }
+
+    async validate(request, APIContext) {
+        try {
+            const { headers } = request
+            const ts = new Date(headers.timestamp)
+            const hash = this._createHash(request, APIContext, ts)
+            return hash === headers.hash;
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
+
+    async createSignature(request, APIContext) {
+        try {
+            const ts = new Date()
+            return this._createHash(request, APIContext, ts)
+        } catch (error) {
+            console.log(error)
+            return null
+        }
+
+    }
+}
+
+module.exports = HMACSignature
